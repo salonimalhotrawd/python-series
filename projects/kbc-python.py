45bfde49dcbaa6f4b523b41af9fbbeb4865a855e
Saloni Malhotra <salonimalhotra1ind@gmail.com>
2026-07-05 00:39:23 +0530

    Add Project folder to create the projects

---
+++ projects/kbc-python.py
@@ -0,0 +1,9 @@
+# @Program: KBC Projects in Python
+# @Author: Saloni Malhotra
+# @Date: 05-7-2027
+
+# @description:
+#     Create a program capable of displaying questions to the user like KBC
+#     Use List data type to store the questions and their correct answers
+#     Display the final amount the person is taking home after playing the game
+
